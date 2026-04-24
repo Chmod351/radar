4ee57d6893d751ba043cd0e772f5e434c2349c94
Chmod351 <yamil.t.351@proton.me>
2026-04-24 13:54:44 -0300

    logica de parseo del dashboard

---
+++ src/parsers/dashboardParsers.ts
@@ -0,0 +1,19 @@
+import type { Technology } from "../shared/types";
+import { noise } from "../shared/utils/data";
+
+export const realTechFilter = (t: Technology) => {
+ 
+  return !noise.some(n => t.name.includes(n));
+};
+
+
+export const infraTranslatorForDashboard=(infraStatus:number)=>{
+  const status=["ERR","N/A","MANG","VULN","REV-REQ","SEC","GEN"];
+
+  return status[infraStatus];
+};
+
+export const cdnTranslatorForDashboard=(value:number)=>{
+  const cdnProvider=["NN","C-FLARE","AKAMAI","C-FRONT","FASTLY","INCAP","G-CDN","GITHUB_CDN","AWS_S3", "UNKNOWN_CDN"];
+  return cdnProvider[value];
+};
