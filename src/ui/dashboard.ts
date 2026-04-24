9eab0c14f5d9ec55a2d7df01e394aff4766f1d9f
Chmod351 <yamil.t.351@proton.me>
2026-04-24 13:54:37 -0300

    corregi un bug en el dashboard que no mostraba correctamente el tipo de infraestructura y separe la logica de parseo

--- src/ui/dashboard.ts
+++ src/ui/dashboard.ts
@@ -1,25 +1,8 @@
-import type { AnalyzedTarget, Technology } from "../shared/types";
+import type { AnalyzedTarget } from "../shared/types";
 import { calculatePriority } from "../domain/calculatePriority.ts";
 import { calculateStatus, getServerInfo } from "../domain/calculateStatus.ts";
-import { noise } from "../shared/utils/data.ts";
 import { SENSORS } from "../shared/utils/const.ts";
-
-const REAL_TECH_FILTER = (t: Technology) => {
- 
-  return !noise.some(n => t.name.includes(n));
-};
-
-
-const infraTranslatorForDashboard=(infraStatus:number)=>{
-  const status=["ERR","N/A","MANG","VULN","REV-REQ","SEC","GEN"];
-
-  return status[infraStatus];
-};
-
-const cdnTranslatorForDashboard=(value:number)=>{
-  const cdnProvider=["NN","C-FLARE","AKAMAI","C-FRONT","FASTLY","INCAP","G-CDN","GITHUB_CDN","AWS_S3", "UNKNOWN_CDN"];
-  return cdnProvider[value];
-};
+import { cdnTranslatorForDashboard, infraTranslatorForDashboard, realTechFilter } from "../parsers/dashboardParsers.ts";
 
 /**
  * Renderiza la tabla final en la terminal.
@@ -36,7 +19,7 @@ export function dashboard(finalReport: AnalyzedTarget[]): void {
     const intel = item.http_intel || {};
     // FILTRAMOS AQUÍ: Solo lo que no sea ruido
     const rawStack = item.http_stack || [];
-    const cleanStack = rawStack.filter(REAL_TECH_FILTER);
+    const cleanStack = rawStack.filter(realTechFilter);
     const portsSummary = item.open_ports && item.open_ports.length > 0
       ? item.open_ports.map(p => `${p.port}/${p.service}`).join(", ")
       : "--";
@@ -50,7 +33,6 @@ export function dashboard(finalReport: AnalyzedTarget[]): void {
     const infraStatus=infraTranslatorForDashboard(item.infra_status||0);
     let cdnProvider;
     if (item.cdn) {
-      
       cdnProvider= cdnTranslatorForDashboard(item.cdn);
     }
 
@@ -62,15 +44,13 @@ export function dashboard(finalReport: AnalyzedTarget[]): void {
       status: realStatus && item.action ===SENSORS.ACTION.DUPLICATE ? realStatus + "/D": realStatus,
       priority: priorityLabel,
       HSTS: (intel.error === "Unreachable") ? "--" : (sec.hsts ? "✔️" : "❌"),
-      // app: item.app_status,
       cdn: cdnProvider || "??" ,
       tech: techSummary.substring(0,20),
       asn:item.asn_owner,
       ip:item.ip,
-      type:item.infra_type===2?"S-Host": (item.infra_type?"Cloud":"Unk"),
-      infra: infraStatus,
-      server: serverInfo.slice(0, 15),
-      ports:portsSummary,
+      type:item.infra_type===2 ? "S-Host": (item.infra_type===1 ?"Cloud" : "Unk"),
+      infra: infraStatus, 
+      server: serverInfo.slice(0, 15), ports:portsSummary,
     };
   });
 
