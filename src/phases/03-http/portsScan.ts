1fd1facfeba5c6ce1f927508766beb0633ff23be
Chmod351 <yamil.t.351@proton.me>
2026-04-24 12:35:37 -0300

    separe logica de nmap

--- src/phases/03-http/portsScan.ts
+++ src/phases/03-http/portsScan.ts
@@ -1,7 +1,8 @@
 import { execa } from "execa";
-import { logger } from "../../shared/errorLogger.ts";
 import type { OpenPort } from "../../shared/types.ts";
-import { getErrorMessage, PROTOCOLS } from "../../shared/utils.ts";
+import { logger } from "../../shared/systemLogger.ts";
+import { getErrorMessage } from "../../shared/utils/utils.ts";
+import { parseNmapOutput } from "../../parsers/parserNmap.ts";
 
 
 
@@ -27,45 +28,6 @@ async function runWithNmapLimit<T>(fn: () => Promise<T>): Promise<T> {
 
 
 
-
-function parseNmapOutput(stdout: string): OpenPort[] {
-  const ports: OpenPort[] = [];
-  const lines = stdout.split("\n");
-
-  for (const line of lines) {
-    const match = line.match(/^(\d+)\/(tcp|udp)\s+open\s+(.+)$/);
-    if (match ) {
-      const portNum = parseInt(match[1] || "0", 10);
-      const transportStr = (match[2] || "").toLowerCase(); 
-      const fullServiceString = match[3] ? match[3].trim() : "";
-      
-      const shards=fullServiceString ? fullServiceString.split(/\s+/):[]
-      const serviceRaw=shards && shards[0]? shards[0].toLowerCase() : ""
-      const versionInfo=shards.slice(1).join(" ") || null
-
-      let transportId: number = PROTOCOLS.TRANSPORT.UNKNOWN;
-      if (transportStr === "tcp") transportId = PROTOCOLS.TRANSPORT.TCP;
-      if (transportStr === "udp") transportId = PROTOCOLS.TRANSPORT.UDP; 
-      
-      let appId :number= PROTOCOLS.APP.UNKNOWN;
-      if (serviceRaw.includes("http")) appId = PROTOCOLS.APP.HTTP;
-      if (serviceRaw.includes("ssl") || serviceRaw.includes("https")) appId = PROTOCOLS.APP.HTTPS;
-      if (serviceRaw.includes("ssh")) appId = PROTOCOLS.APP.SSH;
-      if (serviceRaw.includes("ftp")) appId = PROTOCOLS.APP.FTP;
-      if (serviceRaw.includes("dns")) appId = PROTOCOLS.APP.DNS;
-      if (serviceRaw.includes("sql") || serviceRaw.includes("db")) appId = PROTOCOLS.APP.DATABASE;
-
-      ports.push({
-        port:portNum,
-        protocol: appId,
-        transport:transportId,
-         service:serviceRaw,
-         version:versionInfo
-      });
-    }
-  }
-  return ports;
-}
 
 /**
  * Ejecuta un escaneo de puertos rápido y no intrusivo.
