774d8e5838068e7a8289a20709de4932c3e2f45b
Chmod351 <yamil.t.351@proton.me>
2026-04-24 08:21:34 -0300

    añadi comentario

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -2,7 +2,7 @@ import { logger } from "./errorLogger";
 import type { AnalyzedTarget } from "./types";
 
 
-
+// bun ya maneja user agents, toca migrar esto al binario de bun
 export const USER_AGENTS :string[]= [
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
@@ -11,6 +11,13 @@ export const USER_AGENTS :string[]= [
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
   "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
 ];
+
+export const chromeHints = [
+  "-H", 'sec-ch-ua: "Not(A:Brand";v="99", "Google Chrome";v="142", "Chromium";v="142"',
+  "-H", "sec-ch-ua-mobile: ?0",
+  "-H", 'sec-ch-ua-platform: "Windows"'
+]
+
 export const noise:string[]= ["UncommonHeaders", "Cookies", "HttpOnly", "Content-Language", 
   "X-Frame-Options", "X-XSS-Protection", "Strict-Transport-Security", 
   "X-Content-Type-Options", "Access-Control-Allow-Methods", 
