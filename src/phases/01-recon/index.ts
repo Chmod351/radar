3d5d2585831eca2766733a3c3c0b5d77afecdc40
Chmod351 <yamil.t.351@proton.me>
2026-05-01 17:14:46 -0300

    elimine el index

--- src/phases/01-recon/index.ts
+++
@@ -1,8 +0,0 @@
-import { logger } from "../../shared/systemLogger.ts";
-import { streamAllSubdomains } from "./subdomainFinder.ts";
-
-export function reconPhase(target:string){
-  logger.info("PHASE-01",`Iniciando reconocimiento de subdominios para ${target}`);
-  const domains= streamAllSubdomains(target); 
-  return domains;
-}
